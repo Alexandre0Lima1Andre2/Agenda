68b742e716c0b81bfe121e89e5ddff938e3b43f3
Alexandre Lima André <alexandre.andre@etec.sp.gov.br>
2025-04-08 14:23:34 -0300

    Update Banco de Dados Agenda.sql

--- Banco de Dados Agenda.sql
+++ Banco de Dados Agenda.sql
@@ -4,13 +4,13 @@ drop table Tarefa;
 create table Tarefa(
 id_tarefa int primary key,
 descricao_tarefa varchar(100),
-data_inico_tarefa date,
+data_inicio_tarefa date,
 data_fim_tarefa date
 );
 
 select * from Tarefa;
 
-insert into Tarefa (id_tarefa,descricao_tarefa,data_inico_tarefa,data_fim_tarefa) values
+insert into Tarefa (id_tarefa,descricao_tarefa,data_inicio_tarefa,data_fim_tarefa) values
 (1, 'Planejar o cronograma do projeto', '2025-04-01', '2025-04-02'),
 (2, 'Desenvolver protótipo inicial', '2025-04-03', '2025-04-07'),
 (3, 'Reunião com equipe de design', '2025-04-08', '2025-04-08'),
